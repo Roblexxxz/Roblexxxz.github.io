a185be5d959434d30c1e82b6247768129c22ac3f
ScriptBox <zaki.coding0119@gmail.com>
2026-04-20 16:22:05 -0700

    Create registry.js

---
+++ Content/Avatar/Assets/registry.js
@@ -0,0 +1,8 @@
+export const AvatarRegistry = {
+    headpieces: [
+        { id: "bacon_hair", name: "Bacon Hair", class: "Headpiece" }
+    ],
+    clothing: [
+        { id: "noob_classic", name: "Noob Standard", class: "Character" }
+    ]
+};
