2a56e15f999786198578476d1cc996b6e1000942
Krontixz <zaki.coding0119@gmail.com>
2026-05-08 12:47:56 -0700

    Update character.js

--- Robloxstudio/Lava_Rising/character.js
+++ Robloxstudio/Lava_Rising/character.js
@@ -7,141 +7,106 @@ export class Survivor {
         this.isAlive = true;
         this.characterGroup = new THREE.Group();
         this.velocity = new THREE.Vector3();
-        this.isGrounded = true;
-        this.gravity = -0.012;
-        this.jumpForce = 0.35;
-        this.moveSpeed = isNPC ? 0.08 : 0.12;
+        this.isGrounded = false;
+        this.gravity = -0.015;
+        this.jumpForce = 0.38;
+        this.moveSpeed = isNPC ? 0.07 : 0.14;
         this.oofSound = new Audio('../../Content/sounds/roblox-ooof-made-with-Voicemod.mp3');
         this.targetPlatform = null;
         this.createModel();
     }
 
     createModel() {
-        const skinMat = new THREE.MeshLambertMaterial({ color: this.isNPC ? 0xead9d9 : 0xffdbac });
-        const shirtMat = new THREE.MeshLambertMaterial({ color: this.isNPC ? 0x333333 : 0x00b2ff });
-        this.head = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), skinMat);
+        const skin = new THREE.MeshLambertMaterial({ color: this.isNPC ? 0xead9d9 : 0xffdbac });
+        const clothes = new THREE.MeshLambertMaterial({ color: this.isNPC ? 0x333333 : 0x00b2ff });
+        this.head = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), skin);
         this.head.position.y = 1.3;
-        this.torso = new THREE.Mesh(new THREE.BoxGeometry(1.2, 1.4, 0.6), shirtMat);
-        this.leftArm = new THREE.Mesh(new THREE.BoxGeometry(0.4, 1.4, 0.4), skinMat);
-        this.leftArm.position.set(-0.8, 0.2, 0);
-        this.rightArm = new THREE.Mesh(new THREE.BoxGeometry(0.4, 1.4, 0.4), skinMat);
-        this.rightArm.position.set(0.8, 0.2, 0);
-        this.leftLeg = new THREE.Mesh(new THREE.BoxGeometry(0.5, 1.4, 0.5), shirtMat);
+        this.torso = new THREE.Mesh(new THREE.BoxGeometry(1.2, 1.4, 0.6), clothes);
+        const leg = new THREE.BoxGeometry(0.5, 1.4, 0.5);
+        this.leftLeg = new THREE.Mesh(leg, clothes);
         this.leftLeg.position.set(-0.3, -1.2, 0);
-        this.rightLeg = new THREE.Mesh(new THREE.BoxGeometry(0.5, 1.4, 0.5), shirtMat);
+        this.rightLeg = new THREE.Mesh(leg, clothes);
         this.rightLeg.position.set(0.3, -1.2, 0);
-        this.characterGroup.add(this.head, this.torso, this.leftArm, this.rightArm, this.leftLeg, this.rightLeg);
+        this.characterGroup.add(this.head, this.torso, this.leftLeg, this.rightLeg);
         this.scene.add(this.characterGroup);
     }
 
-    update(inputKeys, lavaY, worldPlatforms = []) {
+    update(inputKeys, lavaY, platforms, eulerY = 0) {
         if (!this.isAlive) return;
         if (this.isNPC) {
-            this.handleNPCLogic(worldPlatforms);
+            this.handleNPCLogic(platforms);
         } else {
-            this.handleMovement(inputKeys);
+            this.handleMovement(inputKeys, eulerY);
         }
-        this.applyPhysics();
+        this.applyPhysics(platforms);
         this.checkLava(lavaY);
     }
 
-    handleMovement(keys) {
+    handleMovement(keys, eulerY) {
         const move = new THREE.Vector3();
         if (keys['w']) move.z -= 1;
         if (keys['s']) move.z += 1;
         if (keys['a']) move.x -= 1;
         if (keys['d']) move.x += 1;
         if (move.length() > 0) {
-            move.normalize().multiplyScalar(this.moveSpeed);
-            this.characterGroup.position.add(move);
-            this.characterGroup.rotation.y = Math.atan2(move.x, move.z);
+            move.normalize().applyAxisAngle(new THREE.Vector3(0, 1, 0), eulerY);
+            this.characterGroup.position.add(move.multiplyScalar(this.moveSpeed));
         }
     }
 
-    handleNPCLogic(worldPlatforms) {
-        if (!this.targetPlatform || this.characterGroup.position.y > this.targetPlatform.position.y) {
-            const potentialTargets = worldPlatforms.filter(p => p.position.y > this.characterGroup.position.y - 1);
-            if (potentialTargets.length > 0) {
-                this.targetPlatform = potentialTargets.reduce((prev, curr) => {
-                    const distPrev = prev.position.distanceTo(this.characterGroup.position);
-                    const distCurr = curr.position.distanceTo(this.characterGroup.position);
-                    return distCurr < distPrev ? curr : prev;
-                });
+    handleNPCLogic(platforms) {
+        if (!this.targetPlatform || this.characterGroup.position.y > this.targetPlatform.position.y + 0.5) {
+            const up = platforms.filter(p => p.position.y >= this.characterGroup.position.y);
+            if (up.length > 0) {
+                this.targetPlatform = up.reduce((prev, curr) => 
+                    curr.position.distanceTo(this.characterGroup.position) < prev.position.distanceTo(this.characterGroup.position) ? curr : prev
+                );
             }
         }
-
         if (this.targetPlatform) {
-            const direction = new THREE.Vector3().subVectors(this.targetPlatform.position, this.characterGroup.position);
-            const distXZ = new THREE.Vector2(direction.x, direction.z).length();
-            
-            direction.y = 0;
-            direction.normalize();
-            
-            if (distXZ > 0.5) {
-                const move = direction.clone().multiplyScalar(this.moveSpeed);
-                this.characterGroup.position.add(move);
-                this.characterGroup.rotation.y = Math.atan2(direction.x, direction.z);
+            const dir = new THREE.Vector3().subVectors(this.targetPlatform.position, this.characterGroup.position);
+            dir.y = 0;
+            const dist = dir.length();
+            if (dist > 0.3) {
+                dir.normalize().multiplyScalar(this.moveSpeed);
+                this.characterGroup.position.add(dir);
+                this.characterGroup.rotation.y = Math.atan2(dir.x, dir.z);
             }
-
-            if (distXZ < 3.5 && this.targetPlatform.position.y > this.characterGroup.position.y + 0.5) {
-                if (Math.random() > 0.96) this.jump();
-            }
-
-            if (distXZ < 1 && this.isGrounded && Math.random() > 0.98) {
+            if (dist < 4 && this.targetPlatform.position.y > this.characterGroup.position.y + 0.5 && Math.random() > 0.97) {
                 this.jump();
             }
         }
     }
 
-    applyPhysics(worldPlatforms) {
-    this.velocity.y += this.gravity;
-    this.characterGroup.position.y += this.velocity.y;
-
-    this.isGrounded = false;
-    
-    for (let platform of worldPlatforms) {
-        const box = new THREE.Box3().setFromObject(platform);
-        const playerPos = this.characterGroup.position;
-        
-        if (playerPos.x > box.min.x && playerPos.x < box.max.x &&
-            playerPos.z > box.min.z && playerPos.z < box.max.z) {
-            
-            if (playerPos.y >= box.max.y && playerPos.y <= box.max.y + 0.5) {
-                this.characterGroup.position.y = box.max.y;
+    applyPhysics(platforms) {
+        this.velocity.y += this.gravity;
+        this.characterGroup.position.y += this.velocity.y;
+        this.isGrounded = false;
+        const pBox = new THREE.Box3().setFromObject(this.characterGroup);
+        for (let p of platforms) {
+            const b = new THREE.Box3().setFromObject(p);
+            if (pBox.intersectsBox(b) && this.velocity.y < 0 && this.characterGroup.position.y > p.position.y) {
+                this.characterGroup.position.y = b.max.y + 1.4;
                 this.velocity.y = 0;
                 this.isGrounded = true;
                 break;
             }
         }
     }
-}
 
-    jump() {
-        if (this.isGrounded) {
-            this.velocity.y = this.jumpForce;
-            this.isGrounded = false;
-        }
-    }
+    jump() { if (this.isGrounded) { this.velocity.y = this.jumpForce; this.isGrounded = false; } }
 
-    checkLava(lavaY) {
-        if (this.characterGroup.position.y < lavaY + 0.6) {
-            this.die();
-        }
-    }
+    checkLava(lavaY) { if (this.characterGroup.position.y < lavaY + 0.5) this.die(); }
 
     die() {
-    if (!this.isAlive) return;
-    this.isAlive = false;
-    this.oofSound.play().catch(() => {});
-    this.characterGroup.rotation.x = Math.PI / 2;
-    this.characterGroup.position.y -= 0.5;
-
-    if (!this.isNPC) {
-        import('./return.js').then(module => {
-            module.handleDeath();
-        });
+        if (!this.isAlive) return;
+        this.isAlive = false;
+        this.oofSound.play().catch(() => {});
+        this.characterGroup.rotation.x = Math.PI / 2;
+        if (!this.isNPC) {
+            import('./return.js').then(m => m.handleDeath());
+        }
     }
-}
 
     get position() { return this.characterGroup.position; }
 }
