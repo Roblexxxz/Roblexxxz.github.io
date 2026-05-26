9a042b4c13178a948f550e0dd5b2e7cf0bc1231d
Krontixz <zaki.coding0119@gmail.com>
2026-05-26 11:19:31 -0700

    Create controls.js

---
+++ Robloxstudio/Natural_Disaster/controls.js
@@ -0,0 +1,28 @@
+export const Input = {
+    keys: {},
+    euler: { x: 0, y: 0 },
+    cameraMode: '3rd',
+    lookSpeed: 0.035,
+
+    init() {
+        window.addEventListener('keydown', (e) => {
+            const key = e.key.toLowerCase();
+            this.keys[key] = true;
+            if (e.key === '1') this.cameraMode = '1st';
+            if (e.key === '3') this.cameraMode = '3rd';
+        });
+
+        window.addEventListener('keyup', (e) => {
+            const key = e.key.toLowerCase();
+            this.keys[key] = false;
+        });
+    },
+
+    update() {
+        if (this.keys['arrowleft']) this.euler.y += this.lookSpeed;
+        if (this.keys['arrowright']) this.euler.y -= this.lookSpeed;
+        if (this.keys['arrowup']) this.euler.x += this.lookSpeed;
+        if (this.keys['arrowdown']) this.euler.x -= this.lookSpeed;
+        this.euler.x = Math.max(-Math.PI / 2.2, Math.min(Math.PI / 2.2, this.euler.x));
+    }
+};
